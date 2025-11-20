818fa171b3fed96b40db3312c82e05ef075e9b15
Tommaso-Cass <tcassolo.tcs@gmail.com>
2025-11-20 12:25:06 +0100

    altro aggiornamento navbar

--- js/navbarJS.js
+++ js/navbarJS.js
@@ -1,16 +1,17 @@
 
-    let NavBarDropDown = document.querySelector("#navbarNavDropdown");
-let navbarToggler = document.querySelector(".navbar-toggler"); 
+let NavBarDropDown = document.querySelector("#navbarNavDropdown");
+let navbarToggler = document.querySelector(".navbar-toggler");
 
-document.addEventListener("click", function(evento){
-    
+document.addEventListener("click", function (evento) {
 
-  
+
+
     let seMenuAperto = NavBarDropDown.classList.contains("show");
-    
-    let seClickNellaNavbar = evento.target.closest('.navbar'); 
 
-    if(seMenuAperto && !seClickNellaNavbar){
-        navbarToggler.click(); 
+    // target.closet serve per selezionare tutti i i discendenti della nav bar
+    let seClickNellaNavbar = evento.target.closest('.navbar');
+
+    if (seMenuAperto && !seClickNellaNavbar) {
+        navbarToggler.click();
     }
 });
